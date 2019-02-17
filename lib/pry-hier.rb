d4f86e411f87fa731a5dc04cd703a7be865114e3
Paul Sonkoly <sonkoly.pal@gmail.com>
2019-02-17 16:54:59 +0000

    Moved blah inside the command

--- lib/pry-hier.rb
+++ lib/pry-hier.rb
@@ -1,29 +1,33 @@
 require 'pry'
 require 'tty-tree'
 
-def blah(klass=Exception)
-  @errors = ObjectSpace.each_object.select do |obj|
-    obj.is_a?(Class) && obj.ancestors.include?(klass)
-  end
-
-  def children_of(error)
-    @errors.select { |other| other.superclass == error }.sort_by(&:to_s)
-  end
-
-  def hashize(error)
-    children = children_of(error).map(&method(:hashize))
-    children.empty? ? error : { error => children }
-  end
-
-  print TTY::Tree.new(hashize(klass)).render
-end
-
 Pry::Commands.create_command 'hier' do
   description 'Shows descendants of a class: hier [Class|Module]'
 
   def process
     blah(eval(args[0]))
   end
+
+  private
+
+  def blah(klass=Exception)
+    @errors = ObjectSpace.each_object.select do |obj|
+      obj.is_a?(Class) && obj.ancestors.include?(klass)
+    end
+
+    def children_of(error)
+      @errors.select { |other| other.superclass == error }.sort_by(&:to_s)
+    end
+
+    def hashize(error)
+      children = children_of(error).map(&method(:hashize))
+      children.empty? ? error : { error => children }
+    end
+
+    pry
+    output.print TTY::Tree.new(hashize(klass)).render
+  end
+
 end
 
 require 'pry'; binding.pry
